90d1132f481273a945d7c96ad8a1837ffb611153
Tamás Kacsó Gáspár <tkgaspar@yahoo.de>
2021-08-03 22:40:15 +0300

    MOdified UserController to return 401, 200, and added logs

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,24 +22,33 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	public static void setLoggingLevel(ch.qos.logback.classic.Level level) {
+		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+		root.setLevel(level);
+	}
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private CartRepository cartRepository;
+
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
 	}
-	
+
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		User user = new User();
@@ -46,8 +56,21 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
-		userRepository.save(user);
+		try {
+			if (createUserRequest.getPassword().length() < 7 || !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+				log.info("|Attempt to create user has failed|");
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			} else {
+				user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+				userRepository.save(user);
+				log.info("|A new User has been created| @{}@", createUserRequest.getUsername());
+				setLoggingLevel(ch.qos.logback.classic.Level.DEBUG);
+				log.debug("|The username of the user created is| @{}@", createUserRequest.getUsername());
+			}
+		} catch (Exception e) {
+			log.error("|An exception occured of the type| {}", e);
+
+		}
 		return ResponseEntity.ok(user);
 	}
-	
 }
